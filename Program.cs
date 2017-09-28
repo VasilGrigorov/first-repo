ddfa12ec8f375d43f48163ff290953e283120409
VasilGrigorov <32367047+VasilGrigorov@users.noreply.github.com>
2017-09-28 19:50:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) {
+                Console.Write($"{i} ");
+                for (int j = 1; j < i; j++) {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
